055ff85123fff09c51d111ac07625d2c7e5e2815
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2022-03-13 21:34:53 -0600

    Fixed all updateURLs with correct branch name

--- github.com/dark.user.js
+++ github.com/dark.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         GitHub Dark
 // @description  Userscript for GitHub
-// @version      0.0.1
+// @version      0.0.2
 // @author       Boian Berberov
 // @namespace    https://github.com/bberberov/userweb
 // @homepageURL  https://github.com/bberberov/userweb
 // @supportURL   https://github.com/bberberov/userweb/issues
-// @downloadURL  https://raw.githubusercontent.com/bberberov/userweb/master/github.com/dark.user.js
+// @downloadURL  https://raw.githubusercontent.com/bberberov/userweb/core/github.com/dark.user.js
 //
 // @match        https://*.github.com/*
 // @noframes
