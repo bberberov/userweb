6d26d106aaae8900860daa56458260ee19d1dbf3
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2023-09-30 19:47:12 -0600

    Added ServeTheHome No Script userstyle and userscript

---
+++ servethehome.com/noscript.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         ServeTheHome No Script
+// @description  No JavaScript userscript for ServeTheHome
+// @version      0.0.1
+// @author       Boian Berberov
+// @namespace    https://github.com/bberberov/userweb
+// @homepageURL  https://github.com/bberberov/userweb
+// @supportURL   https://github.com/bberberov/userweb/issues
+// @downloadURL  https://raw.githubusercontent.com/bberberov/userweb/core/servethehome.com/noscript.user.js
+//
+// @match        https://www.servethehome.com/*
+// @noframes
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+const elements = document.getElementsByClassName('td-thumb-css') );
+
+for (const element of elements)
+{
+	element.style.backgroundImage='url(' + element.dataset.imgUrl + ')';
+	element.style.opacity='unset';
+}
