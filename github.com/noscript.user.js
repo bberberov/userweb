4ce8838bade6befe0036e0a3053f714d724ce87f
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2024-05-27 16:50:17 -0600

    Added GitHub No Script userscript

---
+++ github.com/noscript.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         GitHub No Script
+// @description  No JavaScript userscript for GitHub
+// @version      0.0.1
+// @author       Boian Berberov
+// @namespace    https://github.com/bberberov/userweb
+// @homepageURL  https://github.com/bberberov/userweb
+// @supportURL   https://github.com/bberberov/userweb/issues
+// @downloadURL  https://raw.githubusercontent.com/bberberov/userweb/core/github.com/noscript.user.js
+//
+// @match        https://github.com/*
+// @noframes
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+const e_include_fragment   = document.getElementsByTagName('include-fragment');
+
+for (const element of e_include_fragment)
+{
+	if ( element.hasAttribute('src') )
+	{
+		parent = element.parentElement;
+
+		if ( element.hasAttribute('loading') )
+		{
+			fetch(
+				new URL( element.getAttribute('src'), 'https://github.com/' )
+			).then(
+				(response) => {
+					if ( !response.ok ) {
+						throw new Error('HTTP error!');
+					}
+
+					return response.text()
+				}
+			).then(
+				(text) => {
+					element.insertAdjacentHTML('afterend', text);
+					parent.remove(element)
+				}
+			);
+		}
+	}
+}
